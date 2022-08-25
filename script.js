19659d71c3800da8c4d5cbd32afb2aa1a40cb352
vladposhenko <98976571+vladposhenko@users.noreply.github.com>
2022-08-25 11:21:20 +0300

    isIsogram

--- script.js
+++ script.js
@@ -36,12 +36,12 @@
 // N is greater than 0
 
 // MY VARIANT
-function maxMultiple(divisor, bound){
-    while(bound != divisor) {
-        if(bound % divisor === 0) return bound
-        bound--
-    }
-}
+// function maxMultiple(divisor, bound){
+//     while(bound != divisor) {
+//         if(bound % divisor === 0) return bound
+//         bound--
+//     }
+// }
 
 // BEST VARIANT
 
@@ -49,4 +49,37 @@ function maxMultiple(divisor, bound){
 //     return Math.floor(bound / divisor) * divisor;
 // }
 
-console.log(maxMultiple(37,200));
+// console.log(maxMultiple(37,200));
+
+
+// TASK 3
+
+// Task: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// OUTPUT: 
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+// MY VARIANT
+
+function isIsogram(str){
+    let arr = str.toLowerCase().split("")
+    let result = []
+    arr.forEach((word) => {
+        if(result.indexOf(word) === -1) result.push(word)
+    })
+    return arr.length > result.length ? false : true
+}
+
+
+console.log(isIsogram("Dermatoglyphics")); // true
+console.log(isIsogram("aba")); // false
+console.log(isIsogram("moOse")); // false
+console.log(isIsogram("isIsogram")); // false
+
+// BEST VARIANT 
+
+// function isIsogram(str){
+//     return new Set(str.toUpperCase()).size == str.length;
+// }
